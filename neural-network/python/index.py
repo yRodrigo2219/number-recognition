7d1b0dd53ed2be636e9efec3e208d829a93b9bac
y2219 <rodrigo2219dg@gmail.com>
2021-06-27 11:57:41 -0300

    Json to Network obj in python

--- neural-network/python/index.py
+++ neural-network/python/index.py
@@ -1,8 +1,12 @@
 import mnist_loader
 import network
+import serialize
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data)
+#net = network.Network([784, 50, 10])
+#net.SGD(training_data, 30, 10, 3.0, test_data)
+
+net = serialize.readJson('networks/network-96_13.json')
+net.SGD(training_data, 1000, 10, 3.0, test_data)
--- neural-network/python/serialize.py
+++ neural-network/python/serialize.py
@@ -1,5 +1,6 @@
 import codecs, json
 import network
+import numpy as np
 
 def saveJson(net, acc = None):
   newNet = network.Network(net.sizes)
@@ -10,6 +11,16 @@ def saveJson(net, acc = None):
   if(acc):
     file_path = "networks/network-" + acc.replace('.', '_') + ".json"
   json.dump(newNet.__dict__, codecs.open(file_path, 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=False, indent=2)
+
+def readJson(file_path):
+  net_text = codecs.open(file_path, 'r', encoding='utf-8').read()
+  net_dict = json.loads(net_text)
+  net = network.Network(net_dict["sizes"])
+  net.biases = [np.array(x) for x in net_dict["biases"]]
+  net.weights = [np.array(x) for x in net_dict["weights"]]
+
+  return net
+  
 
 #with open('network.json', 'w') as outfile:
 #  json.dump(net.__dict__, outfile)
